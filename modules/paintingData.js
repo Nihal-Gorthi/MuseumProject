253b0ac8284df27ff2b4b1f268736101b3544033
Nihal Gorthi <nihal.gorthi@gmail.com>
2026-06-03 00:42:03 -0400

    Add Palace bullet points and speaker notes

--- modules/paintingData.js
+++ modules/paintingData.js
@@ -17,9 +17,10 @@ const exhibitDetails = {
     "Section 1: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets."
   ],
   Palace: [
-    "Section 1: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets.",
-    "Section 2: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets.",
-    "Section 1: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets."
+    "The Palace of the Parthian Kings depicts the ruins of a palace.",
+    "The palace was destroyed through years of conflict. Each new empire wanted to leave their mark on the region, but in doing so they were left with a palace in ruins. We want viewers to take away that sacrifice to achieve recognition may not always be worth it because in the end you might be left with less than you started with.",
+    "We also want to highlight the palace's intimidating nature. Building such an expensive and elaborate work of structure further reinforces the theme that people like rulers go to great lengths to attain glory.",
+    "This power dynamic between individuals vs. government connects to one of our team member's Mock Task 1 project, where we studied government surveillance. We mention this because we wanted to connect these exhibits to the modern world. In today's world, there aren't rulers building elaborate palaces yet governments still try to appear powerful and influential at the cost of individual freedoms.",
   ],
   Toreador: [
     "Section 1: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets.",
@@ -37,6 +38,15 @@ const exhibitDetails = {
 };
 
 const speakerNotes = {
+  Palace: `<p>We chose Palace of the Parthian Kings as a reflection of the cost of fighting for glory. It depicts the ruins of a palace constructed thousands of years ago. Located in the ancient capital city of Ctesiphon, the palace was built by the Parthian empire around two thousand years ago. However, we would like to focus not on its peak but on its gradual decline over the years. Starting with the Roman sack of Ctesiphon in 165 CE, the palace was destroyed through years of conflict. Its stones were even taken and used to build the city of Baghdad. Each new empire wanted to leave their mark on the region, but in doing so they were left with a palace in ruins. We want viewers to take away that sacrifice to achieve recognition may not always be worth it because in the end you might be left with less than you started with.</p>
+
+<p>Another aspect we want to highlight is the palace's intimidating nature. Even today, the structure is still many times the height of any one person, as shown in the picture. Building such an expensive and elaborate work of structure further reinforces the theme that people like rulers go to great lengths to attain glory. This power dynamic between individuals and the government connects to one of our team member's Mock Task 1 project, where we studied government surveillance. We mention this because we wanted to connect these exhibits to the modern world. In today's world, there aren't rulers building elaborate palaces. But through government surveillance, particularly in countries like China, governments still try to appear powerful and influential even if it comes at the cost of individual freedoms.</p>
+
+<hr style="border:none;border-top:1px solid rgba(255,255,255,0.2);margin:12px 0;">
+<p>Source:</p>
+<ul>
+  <li><a href="https://www.britannica.com/place/Ctesiphon-ancient-city-Iraq#ref27030" target="_blank" style="color:inherit">https://www.britannica.com/place/Ctesiphon-ancient-city-Iraq#ref27030</a></li>
+</ul>`,
   Toreador: `<p><i>The Dead Toreador</i>, created in 1864, depicts a Spanish bullfighter lying on the ground after having been defeated. We are exhibiting this work first because it was what inspired us to create the theme for our exhibit. Notably, we observed they are still holding their muleta (bottom left), a cloth used to guide the bull throughout the fight. We believe the muleta in particular represents the human desire to show off our worthiness to be recognized, even if it means taking great risks. At the same time a dangerous bull is charging at the fighter, the muleta is waved around, almost like a flag drawing attention to the thrill of the fight.</p>
 
 <p>We also identified literary connections to <i>Antigone</i>, where the protagonist (also named <i>Antigone</i>) honorably fights for the burial but ends up deceased, similarly to the bullfighter seen in the painting. However, there is one notable difference between the two. While the bullfighter likely died unexpectedly and suddenly, Antigone anticipated her death and laments about her legacy: "Be witnessed for me, denied all pity / Unjustly judged!" (Sophocles 226). Something we would like viewers to think about is how the bullfighter might have reflected on his own tragic fate, if given the chance. Would it be satisfaction in having died honorably, or regret?</p>
@@ -67,6 +77,6 @@ export const paintingData = [
   makeEntry('Queen',    -10, 0.5, -19.8, 0,            'Spain, c. 1650', 'Museo del Prado, Madrid'),
   makeEntry('Sword',      0, 0.5, -19.8, 0,            'Italy, c. 1580', 'Uffizi Gallery, Florence'),
   makeEntry('Mask',      10, 0.5, -19.8, 0,            'France, c. 1720', 'Musée du Louvre, Paris'),
-  makeEntry('Palace', -19.8, 0.5, 0,  Math.PI / 2,    'Persia, c. 1400', 'The British Museum, London'),
+  makeEntry('Palace', -19.8, 0.5, 0,  Math.PI / 2,    ' ', 'National Museum of Asian Art', 'Palace of the Parthian Kings at Ctsiphon, Iraq'),
   makeEntry('Toreador', 19.8, 0.5, 0, -Math.PI / 2,   'Edouard Manet', 'National Gallery of Art', 'The Dead Toreador (1864)'),
 ];
